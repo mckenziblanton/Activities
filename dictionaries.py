0b1dc9f1a82d0576676f281a3f0cc094b868d7a7
McKenzi B <mckenziblanton@gmail.com>
2018-05-05 11:15:34 -0700

    Python Day 3: Completed dictionary activity

---
+++ dictionaries.py
@@ -0,0 +1,18 @@
+lst = ["A", "B", "C"]
+
+item = {"key": 100}
+#dictionary name key
+item2 = {"name": "McKenzi"}
+print(item["key"])
+print(item2["name"])
+
+# dictionay can contain multiple pairs of info
+
+hero = {"name": "Iron Man", "nationality": "United States"}
+
+item3 = {"bag": ["laptop", "usb", "food"], "pocket": [5.00,1.00,"gum"],
+"reddit": {"key": [1,2,3,4]}}
+
+print(item3["bag"])
+print(item3["pocket"])
+print(item3["reddit"]["key"][3])
